632a5704048a75820a878fffeaf0716c17fe2f3c
cezar <titicezar@gmail.com>
2022-07-28 08:59:12 +0300

    Initial commit 2

---
+++ WordleHelp.py
@@ -0,0 +1,34 @@
+import CmdProcessor as cp
+
+print("Welcome to Wordle helper!")
+cmdProcessor = cp.CmdProcessor()
+while True:
+    line = input("> ").lower()
+    args = line.split()
+    cmd = args[0]
+    args.pop(0)
+    if cmd == "quit" or cmd == "exit":
+        if cmdProcessor.processExit():
+            break
+    # Info commands
+    elif cmd == "?" or cmd == "help":
+        cmdProcessor.processHelp(args)
+    elif cmd == "stats":
+        cmdProcessor.processStats(args)
+    # Database commands
+    elif cmd == "add":
+        cmdProcessor.processAdd(args)
+    elif cmd == "remove":
+        cmdProcessor.processRemove(args)
+    elif cmd == "save":
+        cmdProcessor.processSave()
+    # Solve commands
+    elif cmd == "match":
+        cmdProcessor.processMatch(args)
+    elif cmd == "reset":
+        cmdProcessor.processReset(args)
+    elif cmd != "":
+        print("Error: Command not recognized")
+print("Goodbye!")
+
+
